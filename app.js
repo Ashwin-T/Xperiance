14ff57cc7238c3553087bef009696804230d47b2
Ashwin Talwalkar <76803989+Ashwin-T@users.noreply.github.com>
2021-05-02 09:48:31 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#add-questions'); // for inputting form into firebase database
+
+
  
 //frontend javaScript
 window.onscroll = function() {scrollFunction()};
@@ -26,16 +28,66 @@ function visitHelpPage(){
   window.location="help.html";
 }
 //end of Frontend Java Script
-//storage for questions page w Firebase
+
+//Questions page w Firebase
 
 //save data
+const docRef = db.collection('questions');
+const output = document.querySelector('#questionList'); // for output form into firebase database
 
-form.addEventListener('submit', (e) =>{
+//feildName for firebase: questions asked (type: string)  
+
+form.addEventListener('submit', (e)=>{
   e.preventDefault();
-  db.collection('questions').add({
-    questionsAsked: form.questionBox.value,
+  if(form.questionBox.value != ''){
+    docRef.add({
+      questionsAsked: form.questionBox.value,
+
+    }).then(function(){
+      console.log("It worked lol, saved data");
+    })
+  }
     //feild: query.nameOfForm.value,
-  })
   document.getElementById('questionBox').value = '';
-});
+});//this works!
+
+
+//try 5125
+getRealtimeUpdates = function(){
+  docRef.onSnapshot({includeMetadataChanges: true},function(doc){
+    if (doc && doc.exists){
+      const dataQ = doc.data();
+      output.textContent = "Data: " + dataQ.questionsAsked;
+    }
+  })
+}
+
+getRealtimeUpdates(); //what the actual frick it doesnt work
+
+
+
+// retrive and post data 
+/*
+function renderQuestions(doc){
+  let li = document.createElement('li');
+  let questionMessage = document.createElement('span');
+  li.setAttribute('data-id',  doc.id);
+  questionMessage.textContext = doc.data().questionsAsked;
+
+  li.appendChild(questionMessage);
+
+  questionList .appendChild(li);
+  console.log(doc);
+}
+
+db.collection('questions').get().then((snapshot) =>{
+  snapshot.docs.forEach(doc =>{
+    renderQuestions(doc);
+  }) 
+})
+*/
+
+//try 2
+
+
 
